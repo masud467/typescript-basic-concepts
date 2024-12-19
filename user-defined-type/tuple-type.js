05f1404abd48ba6769221a3fbcf6e73d6e340aeb
masud467 <masudpfrnt@gmail.com>
2024-12-19 16:23:04 +0600

    added tuple type concept.

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,8 @@
+// tuple-টাপল
+// mixed data - key, value
+var users;
+users = [23, 'anis'];
+console.log(users);
+users.push(33, 'masud');
+console.log(users);
+console.log(users[0]);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,9 @@
+// tuple-টাপল
+// mixed data - key, value
+
+let users:[number,string]
+users=[23,'anis']
+console.log(users)
+users.push(33,'masud')
+console.log(users)
+console.log(users[0]);
